c6d36f72467f96e9f9e40aaf771dc099f6de3c20
camivelandia04 <camilovelandia.r@gmail.com>
2026-02-26 13:25:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,5 @@ st.header("Esto es un subtitulo")
 
 image = Image.open("snoopy.png")
 st.image(image, caption="snoopy.png")
+texto= st.text_input("Ingresa texto","texto inicial")
+  st.write("El texto que has escrito es", texto)
